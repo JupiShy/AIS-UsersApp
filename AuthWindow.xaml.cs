b8d2ca4d037a43171775dbf5753e9644cf0cd82f
Sofiia <hentaionelove2005@gmail.com>
2024-03-10 19:18:06 +0200

    Add project files.

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Interaction logic for AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+
+            User authUser = null;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                authUser = db.Users.Where(b => b.Login == login && b.Pass == pass).FirstOrDefault();
+            }
+            if (authUser != null)
+            {
+                UserPageWindow userPageWindow = new UserPageWindow();
+                userPageWindow.Show();
+                Close();
+            }
+            else MessageBox.Show("Не авторизовано. Перевірте логін та пароль!");
+        }
+
+        private void Button_Click_Reg(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ UserPageWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Interaction logic for UserPageWindow.xaml
+    /// </summary>
+    public partial class UserPageWindow : Window
+    {
+        public UserPageWindow()
+        {
+            InitializeComponent();
+
+            ApplicationContext db = new ApplicationContext();
+            List<User> users = db.Users.ToList();
+
+            listOfUsers.ItemsSource = users;
+
+            amountOfUsers.Text = "Кількість користувачів: " + users.Count;
+        }
+
+        private void Button_Click_Reg(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
